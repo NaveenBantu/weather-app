fd8f7ea1d63eb295f0b9e3af0bebb98272f53000
NaveenBantu <naveen.bantu1@gmail.com>
2022-07-21 00:02:30 +0200

    Add load Opitons to the AsyncPaginate component

---
+++ src/api.js
@@ -0,0 +1,10 @@
+
+export const getApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '17ebee8472msh09fa87f9c279d59p19a7d9jsn965fd64fbe35',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
